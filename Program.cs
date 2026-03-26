96f7f0c96665c6e1fe06ea64ddbcc1e87a5f6792
aayushdayal <dayalaayush@gmail.com>
2026-03-26 23:38:40 +0530

    Remove production and development check for Swagger.

--- Program.cs
+++ Program.cs
@@ -72,15 +72,13 @@ using (var scope = app.Services.CreateScope())
 }
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment() || app.Environment.IsProduction())
-{
+
     app.UseSwagger();
     app.UseSwaggerUI(c =>
     {
         c.SwaggerEndpoint("/swagger/v1/swagger.json", "UrbanDukan User Service v1");
         c.RoutePrefix = string.Empty;
     });
-}
 
 app.UseHttpsRedirection();
 
